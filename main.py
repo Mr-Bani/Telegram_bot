9e541f311813001c52e143a2ae27a2fe74cbb98a
Mr-Bani <87127371+Mr-Bani@users.noreply.github.com>
2021-09-29 15:49:59 +0330

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,28 @@ News=News_check()
 
 while True:
     try:
-        if Announcements!=Announcements_Check():
-            tmp=Announcements_Check()
-
+        AnnounceCheck=Announcements_Check()
+        if Announcements!=AnnounceCheck:
+            i=AnnounceCheck[0]
+            link = i[0]
+            title = i[1]
+            img_link = i[2]
+            caption = urllib.parse.quote("🔵#اعلان" + "\n\n" + title + "\n\n\nhttps://iu.edu.sa" + link)
+            urlopen(
+                "https://api.telegram.org/bot2022812772:AAEjZa3mqn3FMcNjIcB5LiRikLD_XFUagnA/sendPhoto?chat_id=@iumedina&photo={}&caption={}".format(
+                    img_link, caption))
     except:
         pass
 
     try:
-        urlopen("https://api.telegram.org/bot2022812772:AAEjZa3mqn3FMcNjIcB5LiRikLD_XFUagnA/sendPhoto?chat_id=@testtest19129&caption=mammad&photo=https://iu.edu.sa/uploads/news/103020.jpg")
+        NewsCheck=News_check()
+        if News!=NewsCheck:
+            link = i[0]
+            img_link = "iu.edu.sa/uploads/news/" + i[0].split("/")[2] + ".jpg"
+            title = i[1]
+            caption = urllib.parse.quote("🔴#الخبر" + "\n\n" + title + "\n\n\nhttps://iu.edu.sa" + link)
+            urlopen(
+                "https://api.telegram.org/bot2022812772:AAEjZa3mqn3FMcNjIcB5LiRikLD_XFUagnA/sendPhoto?chat_id=@iumedina&photo={}&caption={}".format(
+                    img_link, caption))
     except:
         pass
